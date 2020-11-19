5de24863d550e56c2261119ed933a0714fb1d5aa
marcosvega91 <marcomoretti0103@gmail.com>
2020-11-19 11:55:52 +0100

    add image in index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://i.insider.com/5c8045a4d2ce7802a110ce79" alt="funny" />
-      </header>
+      <p>Image from App.js</p>
+      <img src="https://i.insider.com/5c8045a4d2ce7802a110ce79" alt="funny" />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 import {setupWorker, rest} from 'msw'
 import ReactDOM from 'react-dom';
 import App from './App';
-import funny from './funny.png'
+import funnyApp from './funnyApp.png'
+import funnyIndex from './funnyIndex.jpg'
 
-const worker = setupWorker(rest.get('https://i.insider.com/5c8045a4d2ce7802a110ce79', async (_, res, ctx) => {
-  const image = await fetch(funny).then((res) =>
-    res.arrayBuffer(),
-  )
-  return res(
-    ctx.set('Content-Length', image.byteLength.toString()),
-    ctx.set('Content-Type', 'image/jpeg'),
-    ctx.body(image),
-  )
-}))
+const worker = setupWorker(
+  rest.get('https://i.insider.com/5c8045a4d2ce7802a110ce79', async (_, res, ctx) => {
+    const image = await fetch(funnyApp).then((res) =>
+      res.arrayBuffer(),
+    )
+    return res(
+      ctx.set('Content-Length', image.byteLength.toString()),
+      ctx.set('Content-Type', 'image/png'),
+      ctx.body(image),
+    )
+  }),
+  rest.get('https://i.ytimg.com/vi/Zo_Y-n__Cbc/maxresdefault.jpg', async (_, res, ctx) => {
+    const image = await fetch(funnyIndex).then((res) =>
+      res.arrayBuffer(),
+    )
+    return res(
+      ctx.set('Content-Length', image.byteLength.toString()),
+      ctx.set('Content-Type', 'image/jpeg'),
+      ctx.body(image),
+    )
+  })  
+)
 
 worker.start().then(() =>{
   ReactDOM.render(
